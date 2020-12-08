8d5f8b3bc33649ec29fe033b616b681747ecd909
VickyR97 <71253345+VickyR97@users.noreply.github.com>
2020-12-08 14:07:17 +0530

    Create Webserver

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const port = 4000
+const fs = require('fs')
+
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'content-type': 'text/html'})
+    fs.readFile('index.html',(error, data)=>{
+        if(error){
+            res.writeHead(404)
+            res.write('Page not found')
+        }else{
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, (error)=>{
+    if(error){
+        console.log('Something wrong',error)
+    }else{
+        console.log('Server is listening on:',port)
+    }
+})
